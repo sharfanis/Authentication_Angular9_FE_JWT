9a9e5a1262809b1bd1fbc2b5f83c0b08495f75bc
Shabih <Shabih.sharfani@gmail.com>
2020-09-17 19:48:03 -0500

    chore(): Changes to validation logic in the login form
    validation logic on service layer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
@@ -14,9 +15,10 @@ import { RegistrationComponent } from './user/registration/registration.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [UserComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
